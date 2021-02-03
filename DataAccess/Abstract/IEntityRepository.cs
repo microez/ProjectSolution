7e6243da70ca9fc320e9876a4e3802309622cfc5
Omer Ozkan <omerozkan758@hotmail.com>
2021-02-04 00:56:35 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+
+    //where T :
+    //IEntity : IEntity veya IEntity implement eden bir class verilebilir
+    //new() : new'lenebilir olmalı
+    //like where T: class, IEntity,new()
+
+    public interface IEntityRepository<T>
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //veritabanında product ile ilgili yapacağın operasyonları içeren interface
+    public interface IProductDal : IEntityRepository<Product>
+    {
+
+    }
+}
